bac2fae87ffd9e66cc4d530caf146616a1c8f455
sitaram <sitaram@LAPTOP-CDVHSVHO>
2019-06-09 14:04:14 +0530

    venkat

---
+++ src/main/java/com/app/SpringBootProfiles2Application.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProfiles2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootProfiles2Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/app/runner/SmsService.java
@@ -0,0 +1,16 @@
+package com.app.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+@Component
+@Profile("sms")
+public class SmsService implements CommandLineRunner {
+
+	
+	public void run(String... args) throws Exception {
+		System.out.println("smsService");
+
+	}
+
+}
